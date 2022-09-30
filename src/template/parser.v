ea58404dc62a05008bdcc5c3bdfd9ff21060c9d9
Andrey Poprotsky <poprotsky@mail.ru>
2022-09-30 01:22:50 +0100

    Variables interpolation in templates changed to mustache or jinja style - in double braces

--- src/template/parser.v
+++ src/template/parser.v
@@ -4,15 +4,18 @@ import os
 import regex
 
 fn (s &Service) parse(data string) ?string {
-	mut re := regex.regex_opt('\$\\{(env)|(vault):[-_a-zA-Z0-9\\/\\.]+\\}')?
+	mut re := regex.regex_opt('\\{\\{\\s*(env)|(vault)\\s*:\\s*[-_a-zA-Z0-9\\/\\.]+\\s*\\}\\}')?
 	return re.replace_by_fn(data, s.parse_variables)
 }
 
 fn (s Service) parse_variables(re regex.RE, data string, start int, end int) string {
-	parts := data[start + 2..end - 1].split(':')
-	return match parts[0] {
-		'env' { s.get_enviroment_variable(parts[1]) }
-		'vault' { s.get_vault_variable(parts[1]) }
+	parts := data[start + 2..end - 2].split(':')
+	cutset := ' \t\n\r\v\f'
+	source := parts[0].trim(cutset)
+	name := parts[1].trim(cutset)
+	return match source {
+		'env' { s.get_enviroment_variable(name) }
+		'vault' { s.get_vault_variable(name) }
 		else { '' }
 	}
 }
